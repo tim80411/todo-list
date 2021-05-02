b917fe8200d63bf5f247be0c61bd64f210db9631
tim80411 <tim80411@gmail.com>
2021-05-02 13:18:33 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('server sucess')
+})
+
+app.listen(port, () => {
+  console.log(`The server is running on http://localhost:${port}`)
+})
