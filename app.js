08548cbd77c39f110224ccdff394e2d2faa5b91b
tim80411 <tim80411@gmail.com>
2021-05-03 11:46:29 +0800

    feat: add update todo function

--- app.js
+++ app.js
@@ -44,6 +44,26 @@ app.get('/todos/:id', (req, res) => {
 
 })
 
+app.get('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then(todo => res.render('edit', { todo }))
+    .catch(error => console.error(error))
+})
+
+app.post('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  return Todo.findById(id)
+    .then(todo => {
+      todo.name = name
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch(error => console.error(error))
+})
+
 app.get('/todos/new', (req, res) => {
   res.render('new')
 })
