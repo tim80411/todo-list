93b58c2da6411698664fa55ebc186aa3cd330f35
tim80411 <tim80411@gmail.com>
2021-05-11 09:39:22 +0800

    fixed: heroku error form port setting

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const methodOverride = require('method-override')
 const router = require('./routes/index.js')
 require('./config/mongoose.js')
 
-const PORT = process.env.port || 3000
+const PORT = process.env.PORT || 3000
 const app = express()
 
 // set up view engine
