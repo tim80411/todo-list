0955a535eab63d00d23f42d7f9f69c50c9193691
Yiting Liao <tim80411@gmail.com>
2021-05-23 18:19:05 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const session = require('express-session')
 const methodOverride = require('method-override')
 
 const router = require('./routes/index.js')
@@ -14,6 +15,11 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 // set up middleware
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(router)
