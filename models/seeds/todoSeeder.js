3565ff1712bccbf41d6c93a73ecf26071481fb28
Yiting Liao <tim80411@gmail.com>
2021-05-29 17:15:30 +0800

    refactor: modify todoSeeder

--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,11 +1,39 @@
 const Todo = require('../todo')
+const User = require('../user')
+const bcrypt = require('bcryptjs')
+
+if (process.env.NODE_ENV !== 'prodution') {
+  require('dotenv').config()
+}
+
 const db = require('../../config/mongoose')
 
+const SEED_USER = {
+  name: 'root',
+  email: 'root@example.com',
+  password: 'password'
+}
+
 db.once('open', () => {
-  // 基於TodoSchema 的規則建立種子資料
-  for (let i = 0; i < 10; i++) {
-    Todo.create({ name: `name-${i}` })
-  }
-  
-  console.log('done')
+  // 先創造一個USER
+  bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(SEED_USER.password, salt))
+    .then(hash => User.create({
+      name: SEED_USER.name,
+      email: SEED_USER.email,
+      password: hash
+    }))
+    .then(user => {
+      const userId = user._id
+      return Promise
+        .all(Array.from(
+          { length: 10 },
+          (_, i) => Todo.create({ name: `name-${i}`, userId })
+        ))
+    })
+    .then(() => {
+      console.log('done.')
+      process.exit()
+    })
 })
